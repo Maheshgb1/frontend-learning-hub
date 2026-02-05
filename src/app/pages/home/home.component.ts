5e9718b3b60b2ed4a7d75427e9cd165a85ceca06
runtimeRider <80523507+runtimeRider@users.noreply.github.com>
2026-02-05 14:18:34 +0530

    Develop to main (#7)
    * delete favicon
    
    * js first commit (#5)
    
    Co-authored-by: mahesh <work.maheshgb@gmail.com>
    
    * Feature/js part2 (#6)
    
    * js first commit
    
    * js part2
    
    ---------
    
    Co-authored-by: mahesh <work.maheshgb@gmail.com>
    
    ---------
    
    Co-authored-by: mahesh <work.maheshgb@gmail.com>

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,45 +1,41 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-
 export class HomeComponent {
-
   constructor(private router: Router) {}
 
-onHtmlClick() {
-  this.router.navigate(['/html']);
-}
+  onHtmlClick() {
+    this.router.navigate(['/html']);
+  }
 
-onCssClick() {
-  this.router.navigate(['/css']);
-}
+  onCssClick() {
+    this.router.navigate(['/css']);
+  }
 
-onJavascriptClick() {
-  this.router.navigate(['/javascript']);
-}
+  onJavascriptClick() {
+    this.router.navigate(['/javascript']);
+  }
 
-onAngularClick() {
-  this.router.navigate(['/angular']);
-}
+  onAngularClick() {
+    this.router.navigate(['/angular']);
+  }
 
-onBootstrapClick() {
-  this.router.navigate(['/bootstrap']);
-}
+  onBootstrapClick() {
+    this.router.navigate(['/bootstrap']);
+  }
 
-onRxjsClick() {
-  this.router.navigate(['/rxjs']);
-}
+  onRxjsClick() {
+    this.router.navigate(['/rxjs']);
+  }
 
-onNgrxClick() {
-  this.router.navigate(['/ngrx']);
-}
-
+  onNgrxClick() {
+    this.router.navigate(['/ngrx']);
+  }
 }
